1d32e1df6ab68395519510be39d37a73908b004e
Evan Cobb <cobb.evan@gmail.com>
2017-09-04 17:43:43 -0500

    copy-pating someone else's CORS code

--- server.js
+++ server.js
@@ -41,6 +41,12 @@ const reconstructPath = (req, pathvar) => {
     return path
 }
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+});
+
 const port =  process.env.PORT || 3000
 app.listen(port, () => {
     console.log('Listening on', 3000);
